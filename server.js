569d188b420539ac7a9eb17a8f9433404bd02dfc
samuka <samukasp003@gmail.com>
2024-12-30 21:14:50 -0300

    feat: guarnicaoNome aceita varios id

--- server.js
+++ server.js
@@ -42,7 +42,24 @@ app.get('/api/data', async (_, res) => {
 });
 
 app.post('/api/consulta-ponto', upload.none(), async (req, res) => {
-    const { data_inicio, data_final, usuarios, guarnicao,registro_deletado } = req.body;
+    const { data_inicio, data_final, usuarios, guarnicao, registro_deletado } = req.body;
+    console.log('guarnicao:', guarnicao);
+
+    const guarnicoes = {
+        1: 'PMERJ',
+        2: 'BPM',
+        3: 'GAM',
+        4: 'PATAMO',
+        6: 'BOPE',
+        7: 'CHOQUE',
+        8: 'CURSOS',
+        9: 'REUNIAO',
+        10: 'INTRUÇÃO',
+        11: 'COMANDO',
+        12: 'AGUARDNADO PTR',
+        14: 'INCRUSÃO',
+        21: 'COE'
+    };
 
     function dateToMinutes(dateStr) {
         return new Promise((resolve, reject) => {
@@ -57,8 +74,7 @@ app.post('/api/consulta-ponto', upload.none(), async (req, res) => {
         });
     }
 
-
-    async function getHorasUsuario(usuarioId) {
+    async function getHorasUsuario(usuarioId, guarnicaoId) {
         try {
             const response = await axios.post(
                 'https://policia.complexorjbrasil.com.br/php/banco.php?tab=bateponto&acao=relatorio_ponto_datas',
@@ -68,7 +84,7 @@ app.post('/api/consulta-ponto', upload.none(), async (req, res) => {
                     data_inicio: formatDate(data_inicio),
                     data_final: formatDate(data_final),
                     usuario: usuarioId, 
-                    'guarnicao[]': guarnicao,
+                    'guarnicao[]': guarnicaoId, 
                     registro_deletado: registro_deletado || 0,
                 }),
                 {
@@ -81,7 +97,7 @@ app.post('/api/consulta-ponto', upload.none(), async (req, res) => {
             const data = response.data;
             let totalMinutes = 0;
             let nomeUsuario = '';
-            
+
             if (data && data.dados && data.dados.length > 0) {
                 const promises = data.dados.map(async (item) => {
                     const dt_inicio = item.dt_inicio;
@@ -95,7 +111,7 @@ app.post('/api/consulta-ponto', upload.none(), async (req, res) => {
                         totalMinutes += durationMinutes;
                         nomeUsuario = item.nome_usuario;
                     } catch (error) {
-                        console.error('Erro ao calcular a duração para o ponto do usuário', usuarioId,nome_usuario, error);
+                        console.error('Erro ao calcular a duração para o ponto do usuário', usuarioId, nomeUsuario, error);
                     }
                 });
 
@@ -104,23 +120,42 @@ app.post('/api/consulta-ponto', upload.none(), async (req, res) => {
                 const hours = Math.floor(totalMinutes / 60);
                 const minutes = totalMinutes % 60;
 
-                console.log(`Usuário ${usuarioId}: ${hours} horas e ${minutes} minutos.`);
 
-                return { usuarioId,nomeUsuario, totalTempo: `${hours} horas e ${minutes} minutos` };
+                return { 
+                    usuarioId, 
+                    nomeUsuario, 
+                    guarnicaoNome: guarnicoes[guarnicaoId] || 'Guarnição Desconhecida',  
+                    guarnicaoId,
+                    totalTempo: `${hours} horas e ${minutes} minutos` 
+                };
             } else {
-                return { usuarioId,nomeUsuario, totalTempo: 'Nenhum dado encontrado' };
+                return { 
+                    usuarioId, 
+                    nomeUsuario, 
+                    guarnicaoNome: guarnicoes[guarnicaoId] || 'Guarnição Desconhecida',
+                    guarnicaoId,
+                    totalTempo: 'Nenhum dado encontrado' 
+                };
             }
         } catch (error) {
             console.error('Erro ao chamar a API de ponto para o usuário', usuarioId, error.message);
-            return { usuarioId,nomeUsuario, totalTempo: 'Erro ao processar dados' };
+            return { 
+                usuarioId, 
+                nomeUsuario: '', 
+                guarnicaoNome: guarnicoes[guarnicaoId] || 'Guarnição Desconhecida', 
+                guarnicaoId,
+                totalTempo: 'Erro ao processar dados' 
+            };
         }
     }
 
     const resultados = [];
 
     for (const usuarioId of usuarios) {
-        const resultado = await getHorasUsuario(usuarioId);
-        resultados.push(resultado);
+        for (const guarnicaoId of guarnicao) {
+            const resultado = await getHorasUsuario(usuarioId, guarnicaoId);
+            resultados.push(resultado);
+        }
     }
 
     return res.json(resultados);
